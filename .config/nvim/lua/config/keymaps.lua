2058f1b943cd07898b887b41872dc672ea2d9520
Jovana Matkovic <mat.jovana@gmail.com>
2024-10-13 16:08:12 +0200

    add keymap to delete other buffers

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -36,3 +36,20 @@ vim.api.nvim_set_keymap("n", "<leader>tC", "", {
   noremap = true,
 })
 -- End Codeium toggler
+
+-- Close all buffers except current
+-- Close all buffers except current
+vim.api.nvim_create_user_command("BufOnly", function()
+  local current_buf = vim.api.nvim_get_current_buf()
+  -- Get a list of all buffers
+  local buffers = vim.api.nvim_list_bufs()
+  for _, buf in ipairs(buffers) do
+    if buf ~= current_buf and vim.api.nvim_buf_is_loaded(buf) then
+      -- Delete buffer if it's not the current buffer
+      vim.api.nvim_buf_delete(buf, { force = true })
+    end
+  end
+end, {})
+
+-- Map <leader>bo to BufOnly command
+vim.api.nvim_set_keymap("n", "<leader>bo", ":BufOnly<CR>", { noremap = true, silent = true })
